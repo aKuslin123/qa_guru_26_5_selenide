d8ffc8ee55e5e659d5504e758c093ac4c62b9927
aKuslin123 <a.Kuslin123@gmail.com>
2024-04-08 11:17:50 +0700

    добавил @AfterEach

--- src/test/java/tests/DragAndDropTest.java
+++ src/test/java/tests/DragAndDropTest.java
@@ -2,6 +2,8 @@ package tests;
 
 import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.DragAndDropOptions;
+import com.codeborne.selenide.Selenide;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
@@ -15,6 +17,11 @@ public class DragAndDropTest {
         Configuration.baseUrl = "https://the-internet.herokuapp.com";
         Configuration.pageLoadStrategy = "eager";
         Configuration.browserSize = "1920x1080";
+    }
+
+    @AfterEach
+    void afterEach() {
+        Selenide.closeWebDriver();
     }
 
     @Test
--- src/test/java/tests/SolutionsEnterpriseTest.java
+++ src/test/java/tests/SolutionsEnterpriseTest.java
@@ -1,6 +1,8 @@
 package tests;
 
 import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.Selenide;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
@@ -15,6 +17,11 @@ public class SolutionsEnterpriseTest {
         Configuration.baseUrl = "https://github.com/";
         Configuration.pageLoadStrategy = "eager";
         Configuration.browserSize = "1920x1080";
+    }
+
+    @AfterEach
+    void afterEach() {
+        Selenide.closeWebDriver();
     }
 
     @Test
